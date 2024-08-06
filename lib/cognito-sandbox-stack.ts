72a05e79d776afcad238c5cb7be83f64a6250022
konabe <rt.konabe@gmail.com>
2024-08-06 12:23:34 +0900

    CognitoユーザープールとIDプールの作成

--- lib/cognito-sandbox-stack.ts
+++ lib/cognito-sandbox-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 
 export class CognitoSandboxStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      userPoolName: "cognito-sandbox-user-pool",
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      deletionProtection: false, // 実験的な物なので削除可
+    });
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool,
+      userPoolClientName: "cognito-sandbox-user-pool-client",
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userPassword: true,
+        userSrp: true,
+      },
+      authSessionValidity: cdk.Duration.minutes(3),
+      accessTokenValidity: cdk.Duration.minutes(5),
+      refreshTokenValidity: cdk.Duration.minutes(60),
+      idTokenValidity: cdk.Duration.minutes(20),
+      preventUserExistenceErrors: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CognitoSandboxQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const identityPool = new cognito.CfnIdentityPool(this, "IdentityPool", {
+      identityPoolName: "cognito-sandbox-identity-pool",
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
   }
 }
