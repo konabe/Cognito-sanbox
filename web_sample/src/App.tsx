36b4634927235aa18fb847f09b3f3453d2a338e4
konabe <rt.konabe@gmail.com>
2024-08-06 16:00:56 +0900

    ページをオリジナルに変更

--- web_sample/src/App.tsx
+++ web_sample/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { fetchUserAttributes, signIn, signOut } from "@aws-amplify/auth";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "@aws-amplify/ui-react/styles.css";
 import "./App.css";
 
 // ref. https://docs.amplify.aws/react/build-a-backend/auth/use-existing-cognito-resources/#use-auth-resources-without-an-amplify-backend
@@ -35,38 +33,91 @@ Amplify.configure({
     },
   },
 });
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <>
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [username, setUsername] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
+
+  const clickSignOut = async () => {
+    await signOut();
+  };
+
+  const updateAuthSession = async (authStatus: string) => {
+    if (authStatus === "authenticated") {
+      // NOTE: useAuthenticatorを使うと状態が変化しても自動的に反映してくれない
+      const userAttributes = await fetchUserAttributes();
+      setUsername(userAttributes.sub ?? "無し");
+      setUserEmail(userAttributes.email ?? "無し");
+    }
+  };
+
+  useEffect(() => {
+    updateAuthSession(authStatus);
+  }, [authStatus]);
+
+  const clickLogin = async () => {
+    try {
+      await signIn({
+        username: email,
+        password: password,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return authStatus === "authenticated" ? (
+    <>
+      <h1>ユーザー画面</h1>
+      <div>
+        <div>
           <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+            <b>username</b>
           </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
+          <div>{username}</div>
+        </div>
+        <div>
+          <div>
+            <b>email</b>
           </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </>
-      )}
-    </Authenticator>
+          <div>{userEmail}</div>
+        </div>
+      </div>
+      <button onClick={clickSignOut}>ログアウトする</button>
+    </>
+  ) : (
+    <>
+      <div>
+        <h1>ログインする</h1>
+        <form>
+          <label>
+            メールアドレス
+            <input
+              type="text"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            パスワード
+            <input
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button type="button" onClick={clickLogin}>
+            ログイン
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
